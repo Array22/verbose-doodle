8f9aed4a91ff6a2376dd96b0b4be3723574e1328
Adam <143813907+Array22@users.noreply.github.com>
2026-04-08 19:50:49 +1000

    feat: Added convert_dollars logic which works up to 4 figits

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -9,43 +9,71 @@ double_digit = data["double_digit"]
 tens = data["tens"]
 zeros = data["zeros"]
 
-# class Cents:
-#     def __init__():
-#         pass
-    
-def convert_cents(x: str):
-    """Convert decimal part of a number to words."""
+def convert_2digit(x: str):
+    """Convert a 2 digit number to words."""
     if len(x) > 2:
         raise ValueError("Please round number to 2 decimal places")
     if len(x) == 1: #e.g. .2
-        return f"{tens[x]} CENTS"
+        return tens[x]
     values = []
     values.append(tens[x[0]])
     values.append(ones[x[1]])
-    cents_worded = " ".join(values)
+    words = " ".join(values)
     if x[0] == "1": #e.g. .11, .14
-        cents_worded = double_digit[x]
-    return f"{cents_worded} CENTS"
+        words = double_digit[x]
+    return words
     
+def convert_cents (x: str):
+    """Convert decimal part of a number to cents in words"""
+    words = convert_2digit(x)
+    return f"{words} CENTS"
 
 def convert_dollars(x: str):
-    """Convert whole part of number to words."""
-    
-    pass
+    """Convert whole part of number to dollars in words."""
+    reversed = x[::-1] #reverse indices, e.g 123 -> 321
+    i = len(reversed) - 1
+    words_list = []
+    while i > -1:
+        if i % 3 == 1:
+            # print(reversed[i-1:i+1][::-1])
+            words_list.append(convert_2digit(reversed[i-1:i+1][::-1]))
+            j = i - 2
+        else:
+            words_list.append(ones[reversed[i]]) #append value
+        if i > 1:
+            words_list.append(zeros[str(i)]) # append zeroes based on index
+            words_list.append("AND")
+
+        i -= 1
+        if i+1 % 3 == 1:
+            i = j
+    words = " ".join(words_list)
+    return words
 
 def convert_money(x: str):
-    """Convert a number to word."""
+    """Convert a number to monetary value in words."""
     try:
         float(x)
     except ValueError:
         print("Please input a numerical number.")
     if "." in x:
         try:
-            part_whole, part_int = x.strip().strip("0").split(".")
+            part_dollars, part_cents = x.strip().strip("0").split(".")
         except ValueError:
             print(f"{x} is not a valid number")
+    a = convert_cents(part_cents)
+    b = convert_dollars(part_dollars)
+    return f"{a} AND {b}"
 
 if __name__ == "__main__":
     user_input = input("Please input number: ")
-    print(convert_cents(user_input))
+    # # # print(convert_cents(user_input))
+    print(convert_dollars(user_input))
+
+    # x = "123"
+    # reversed = "321"
+    # text = reversed[0:2][::-1]
+    # print(text)
+    # print(convert_2digit(text))
+
     pass
