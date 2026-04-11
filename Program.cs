971d7afb6eab469621ef385e76ee351e4d706d64
Adam <143813907+Array22@users.noreply.github.com>
2026-04-11 23:26:20 +1000

    Added starting C# code

--- Program.cs
+++ Program.cs
@@ -1,15 +1,45 @@
 ﻿
-class Person
+interface IPerson
 {
-    public Person()
-    {
-        
+    string Height {get;}
+}
+
+interface IHuman
+{
+    int Legs {get;}
+}
+
+class Person : IPerson, IHuman
+{ 
+    public int Legs {get; set;} = 2;
+    public string Height {get; set;} = "170";
+}
+
+// class Money {
+//     public string ConvertCents() {
+
+//     }
+// }
+
+class Input
+{
+    public static void TestFunction(){
+        Console.Write("Please input a value: ");
+        string value = Console.ReadLine() ?? "";
+        Console.WriteLine(value);
     }
+    
 }
 
 class Program {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string value = "123.";
+        string[] parts = value.Split('.');
+        string a = parts[0]; 
+        string b = parts[1];
+        Console.WriteLine($"{a} AND {b}");
+
+
     }
 }
