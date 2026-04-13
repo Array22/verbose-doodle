84b8b2b4ee4ca4c8daabc5f696ff27fb0411d748
Adam <143813907+Array22@users.noreply.github.com>
2026-04-13 23:30:44 +1000

    Added: Money Class

--- Program.cs
+++ Program.cs
@@ -1,45 +1,39 @@
 ﻿
-interface IPerson
-{
-    string Height {get;}
-}
-
-interface IHuman
-{
-    int Legs {get;}
-}
-
-class Person : IPerson, IHuman
+class Digit
 { 
     public int Legs {get; set;} = 2;
     public string Height {get; set;} = "170";
 }
 
-// class Money {
-//     public string ConvertCents() {
+class Money {
+
+    public string Dollar {get; set;} = "";
+    public string Cents {get; set;} = "";
+    public bool IsWhole {get; set;} = false;
+    public string ConvertCents() {
+
+    }
+}
+
+// class Input
+// {
+//     public static void TestFunction(){
+//         Console.Write("Please input a value: ");
+//         string value = Console.ReadLine() ?? "";
+//         Console.WriteLine(value);
+//         string value = "123.";
+//         string[] parts = value.Split('.');
+//         string a = parts[0]; 
+//         string b = parts[1];
+//         Console.WriteLine($"{a} AND {b}");
 
 //     }
 // }
 
-class Input
-{
-    public static void TestFunction(){
-        Console.Write("Please input a value: ");
-        string value = Console.ReadLine() ?? "";
-        Console.WriteLine(value);
-    }
-    
-}
-
 class Program {
     static async Task Main(string[] args)
     {
-        string value = "123.";
-        string[] parts = value.Split('.');
-        string a = parts[0]; 
-        string b = parts[1];
-        Console.WriteLine($"{a} AND {b}");
-
+        
 
     }
 }
