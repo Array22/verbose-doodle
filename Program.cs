d81eed69b85adf80b6f11f8e0531cd1bb2a28d55
Adam <143813907+Array22@users.noreply.github.com>
2026-04-15 19:30:13 +1000

    Added input parsing logic

--- Program.cs
+++ Program.cs
@@ -1,17 +1,25 @@
 ﻿using System.Text.Json;
 
+// var jsonObject = File.ReadAllText("map.json");
+// var map = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string,string>>>(jsonObject);
+Input.TestFunction();
+
+
+// Money test = new() {Value = "23"};
+// Console.WriteLine(test.Cents);
+
 
 class DigitMap
 { 
-    public int Legs {get; set;} = 2;
-    public string Height {get; set;} = "170";
+    public required Dictionary<string, string> MapOnes {get; set;}
+    public required Dictionary<string, string> MapTens {get; set;}
+    public required Dictionary<string, string> MapZeros {get; set;}
 }
 
 class Money {
 
-    public string Dollar {get; set;} = "";
+    public float Value {get; set;} = 0;
     public string Cents {get; set;} = "";
-    public bool IsWhole {get; set;} = false;
     public string ConvertCents() {
         throw new NotImplementedException();
     }
@@ -22,24 +30,14 @@ class Input
     public static void TestFunction(){
         Console.Write("Please input a value: ");
         string Value = Console.ReadLine() ?? "";
-        Console.WriteLine(Value);
+        if (float.TryParse(Value, out float result) == false)
+        {
+            throw new ArgumentException("Please input a valid number.");
+        }
+        Console.WriteLine(result);
         string[] Word = Value.Split('.');
         string a = Word[0]; string b = Word[1];
         Console.WriteLine($"{a} AND {b}");
-
-    }
-}
-
-class Program {
-    public static void Main(string[] args)
-    {
-        var jsonObject = File.ReadAllText("map.json");
-        var map = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(jsonObject);
-        // Input.TestFunction();
-        if (map != null)
-        {
-            Console.WriteLine(map["ones"]);
-        }
 
     }
 }
